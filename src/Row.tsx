68778aae1a9c8416bc89b7486ce3b26547bc7bf7
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-08-19 00:21:13 +0530

    css added

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import axios from './axios.ts'
-
+import '../public/style/row.css'
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
@@ -9,35 +9,42 @@ interface RowProps {
     fetchUrl: {}
 }
 
+interface Movie {
+    id: number, 
+    name: string,
+    poster_path: string
+}
+
 const Row: React.FC<RowProps> = ({ title, fetchUrl }) => {
 
-    const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState<Movie []>([])
 
     useEffect: React.useEffect(() => {
-         async function fetchData()  {
+        async function fetchData() {
             const request = await axios.get(fetchUrl)
             console.table(request.data.results)
-           setMovies(request.data.results)
+            setMovies(request.data.results)
             return request
         }
-        
-      fetchData()
+
+        fetchData()
     }, [fetchUrl])
 
     console.log(movies)
 
-  return (
-    <div className="row"> 
-       <h2>{title }</h2>  
+    return (
+        <div className="row">
+            <h2>{title}</h2>
 
-       <div className="row_posters">
-         
-        {movies.map(movie => (
-            <img src={`${base_url}${movie.poster_path}`} alt={movie.name} className="src" />
-        ))}
-       </div>
-    </div>
-  )
+            <div className="row_posters">
+
+                {movies.map(movie => (
+                    <img className="row_poster"
+                        src={`${base_url}${movie.poster_path}`} alt={movie.name}  />
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Row
